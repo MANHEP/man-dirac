b747b5b2c3f33d410df80bc5c8fe9e0536c57279
Andrew McNab <andrew.mcnab@cern.ch>
2017-09-27 20:28:40 +0100

    Work with OutputData if enabled

--- tarJob.sh
+++ tarJob.sh
@@ -8,9 +8,11 @@ printenv
 # $2 has the full LFN of the file this job has to process
 # The file is downloaded to the job working directory by DIRAC
 
+# $3 is the unique DIRAC Job ID
+
 # Get just the filename from the full LFN
 localfile=`echo $2 | sed 's:^.*/::'`
 tar xvf $localfile
 
 # The file in each tar file is always called data.txt
-wc data.txt > wc.log
+wc data.txt > $3.wc.log
